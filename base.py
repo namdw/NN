f1cdee89e23eca7349606f9d84a51e6e2bb2b49d
namdw <dnjsxodp@gmail.com>
2016-12-22 17:33:10 +0900

    first working model without bias term

--- base.py
+++ base.py
@@ -24,7 +24,7 @@ class NN():
 				self.numY = args[1]
 				if(len(args)>2):
 					self.numW = args[2]
-		self.numH = len(self.numW)-1 # number of hidden layers
+		self.numH = len(self.numW) # number of hidden layers
 
 		## Initialize input, weigths, and outputs of the network
 		self.X = np.zeros([1, self.numX])
@@ -36,6 +36,12 @@ class NN():
 				self.W[i] = np.random.rand(self.numW[i-1], self.numY)
 			else:
 				self.W[i] = np.random.rand(self.numW[i-1], self.numW[i])
+		self.B = [0] * (len(self.numW)+1)
+		for i in range(len(self.B)):
+			if (i==len(self.B)-1):
+				self.B[i] = np.random.rand(1,self.numY)
+			else:
+				self.B[i] = np.random.rand(1,self.numW[i])
 		self.Y = np.zeros([self.numY,1])
 
 	'''
@@ -45,7 +51,7 @@ class NN():
 	def sigmoid(self, X):
 		result = np.zeros(X.shape)
 		for i in range(len(X[0])):
-			result[0][i] = 1 / (1+math.exp(-1*X[0][i]))
+			result[0][i] = 1 / (1+math.exp(-1*X[0][i])) # sigmoid function 
 		return result
 
 	'''
@@ -55,25 +61,28 @@ class NN():
 	def forward(self, X):
 		a = np.array([X])
 		for i in range(len(self.W)):
-			z = np.dot(a, self.W[i])
-			a = self.sigmoid(z)
+			z = np.dot(a, self.W[i]) / np.prod(a.shape) + self.B[i] # z = a*w + b
+			a = self.sigmoid(z) # a = sigma(a*w)
 		return a
 
 	def train(self, X, Y):
 		X = np.array([X])
 		Y = np.array([Y])
-		n = 0.5
+		n = 1.0
 		A = [0]*(len(self.W)+1)
 		Z = [0]*len(self.W)
 		A[0] = X
 		for i in range(len(self.W)):
-			Z[i] = np.dot(A[i], self.W[i])
-			A[i+1] = self.sigmoid(Z[i])
+			Z[i] = np.dot(A[i], self.W[i]) / np.prod(A[i].shape) + self.B[i] # z = a*w + b
+			A[i+1] = self.sigmoid(Z[i]) # a = sigma(a*w)
 		D = [0]*(len(self.W)+1)
-		D[0] = 0.5 * (A[-1]-Y)**2 * A[-1]*(1-A[-1])
+		D[0] = 0.5 * (A[-1]-Y)**2 / np.prod(A[-1].shape) * A[-1]*(1-A[-1])
 
 		for i in range(len(self.W)):
-			D[i+1] = np.transpose(np.dot(self.W[len(self.W)-i-1], np.transpose(D[i])))
+			D[i+1] = np.transpose(np.dot(self.W[len(self.W)-i-1], np.transpose(D[i]))) * A[-2-i]*(1-A[-2-i])
 			self.W[-1-i] = self.W[-1-i] + n * np.dot(np.transpose(A[-2-i]), D[i])
+			self.B[-1-i] = self.B[-1-i] + n * D[i]
+
+		# print(D)
 
 			
--- test_NN.py
+++ test_NN.py
@@ -11,12 +11,12 @@ print("number of nodes : ", testNet.numX, testNet.numW, testNet.numY)
 ## do forward calculation on network
 print("First forward : ",testNet.forward([1,1]))
 
-print("weights : ", testNet.W)
+# print("weights : ", testNet.W)
 
 X = [1,1]
-Y = [1,1]
+Y = [1,0]
 
-for i in range(1000):
+for i in range(10):
 	testNet.train(X,Y)
 
 
